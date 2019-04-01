74203839e9a21f8be716bb3e7a4dd77f754edaa0
YYK12138 <yyk12138@outlook.com>
2019-04-01 19:44:03 +0800

    第三次更新，更新界面UI

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,7 +4,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    imgUrls: [
+      'http://2c.zol-img.com.cn/product/100/472/ceMlIL7Ki7utk.jpg',
+      'http://2e.zol-img.com.cn/product/100_500x2000/916/cefFOl4t2vWDc.jpg',
+      'https://www.jiqizhixin.com/wp-content/uploads/2015/10/100475_web.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    title:["实验室设备使用规范","实验室物品赔偿方案","实验室使用费用计算方法","实验室主要负责热联系方式","实验室预约系统小程序上线"],
+    date: ["2019/3/31","2019/3/27","2019/3/14","2019/2/24","2019/2/1"]
   },
 
   /**
