205d387475e3d53aa5d7bd9f421313a5d06463f3
YYK12138 <yyk12138@outlook.com>
2019-04-02 19:46:49 +0800

    管理端新增实验室概况界面

---
+++ miniprogram/pages/overview/index.js
@@ -0,0 +1,77 @@
+// miniprogram/pages/overview/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name1:"测试中心1",
+    name2: "测试中心2",
+    name3: "测试中心3",
+    open1:"已开放",
+    open2: "已开放",
+    open3: "已开放",
+    num1:"40",
+    num2: "30",
+    num3: "50",
+    margin1:"30",
+    margin2: "12",
+    margin3: "14",
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
