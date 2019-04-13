c8fdd0667e3666bdd1002ec88660e431203695ff
YYK12138 <49021532+YYK12138@users.noreply.github.com>
2019-04-14 01:33:15 +0800

    Add files via upload

--- miniprogram/pages/sysManage/updateUnit/index.js
+++ miniprogram/pages/sysManage/updateUnit/index.js
@@ -1,18 +1,34 @@
 // miniprogram/pages/sysManage/updateUnit/index.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    unit1:"测试单位1",
+    unit2: "测试单位2",
+    unit3: "测试单位3",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    db.collection('testUnit').where({
+      _openid: 'o-euW5HJd4-TLZhvWktMfy0NTf34',
+    })
+      .get({
+        success(res) {
+          console.log(res.data)
+          that.setData({
+            unit1: res.data[0].Tname,
+            unit2: res.data[1].Tname,
+            unit3: res.data[2].Tname
+          })
+        }
+      })
   },
 
   /**
--- miniprogram/pages/sysManage/updateUnit/update/selectUpdate/index.js
+++ miniprogram/pages/sysManage/updateUnit/update/selectUpdate/index.js
@@ -1,18 +1,21 @@
 // miniprogram/pages/sysManage/updateUnit/update/selectUpdate/index.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    TID:"0"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      TID:options.id
+    })
   },
 
   /**
@@ -61,6 +64,45 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+
+  },
+
+  updateUnit:function(e){
+t
+
+  },
+
+  updateUnit:function(e){
+    var tName=e.detail.value.tname
+    var aNmae = e.detail.value.aname
+    var aID = e.detail.value.aid
+    var aPhone = e.detail.value.aphone
+    var position = e.detail.value.position
+    if (tName.length == 0 || aNmae.length == 0 || aID.length == 0 || aPhone.length == 0 || position.length == 0){
+      wx.showToast({
+        title: '以上内容不能为空',
+      })
+    }else{
+      db.collection('testUnit').where({
+        TID:this.data.TID
+      })
+        .get({
+          success(res) {
+            db.collection('testUnit').doc(res.data[0]._id).update({
+              data: {
+                Tname:tName,
+                Aname:aName,
+                AID:aID,
+                Aphone:aPhone,
+                Position:position
+              },
+              success(res) {
+                console.log(res.data)
+              }
+            })
+          }
+        })
+    }
 
   }
 })
